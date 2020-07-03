bbbe10ecdafdb6409c600f969b4ab4712e1a302f
trin0491 <r.priestley@comanduk.co.uk>
2020-07-03 12:02:34 +0100

    Initialise openfin

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,22 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+function initWithOpenFin(): void {
+  // Your OpenFin specific code to go here...
+  const system = fin.desktop.System;
+  system.getAllWindows((windowInfoList) => {
+    windowInfoList.forEach((winInfo) => {
+      fin.desktop.System.showDeveloperTools(winInfo.uuid, winInfo.mainWindow.name);
+    });
+  });
+}
+
+if (typeof fin !== 'undefined') {
+  fin.desktop.main(() => {
+    initWithOpenFin();
+  });
+}
+
 if (environment.production) {
   enableProdMode();
 }
